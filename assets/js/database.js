4f1be15d7b50d487082c0282301163a2e1419a78
Ubuntu <ubuntu@ip-172-31-41-232.ec2.internal>
2022-03-02 04:35:40 +0000

    Adding while loop

--- assets/js/database.js
+++ assets/js/database.js
@@ -124,7 +124,7 @@ window.database = {
     "20-bash-while-loop-html": {
       "title": "Bash - Infinite While Loop",
       "category": "",
-      "content": "While LoopInfinite:while :do  echo \"[CTRL+C] to break\"  sleep 1doneOne liner:while :; do echo \"[CTRL+C] to break\"; sleep 1; done",
+      "content": "Infinite While LoopInfinite:while :do  echo \"[CTRL+C] to break\"  sleep 1doneOne liner:while :; do echo \"[CTRL+C] to break\"; sleep 1; done",
       "url": "/20-bash-while-loop.html",
       "href": "/20-bash-while-loop.html"
     }
